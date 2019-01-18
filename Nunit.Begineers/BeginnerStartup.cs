594a18564c3c712ba3209dc35db190c2c978d24e
smitha <smithajojo@gmail.com>
2019-01-18 19:02:35 +0000

    #writing test to output window #ordering test cases

--- Nunit.Begineers/BeginnerStartup.cs
+++ Nunit.Begineers/BeginnerStartup.cs
@@ -15,4 +15,6 @@ namespace Nunit.Begineers
 
         }
     }
+
+
 }
---
+++ Nunit.Begineers/TimeSheetTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nunit.Begineers
+{
+    class TimeSheetTest
+    {
+        [TestCase,Order(1)]
+        public void CreateTimeSheet()
+        {
+            TestContext.Progress.WriteLine("Executing CreateTimeSheet test cases");
+        }
+
+        [TestCase, Order(2)]
+        public void SubmitTimeSheet()
+        {
+            TestContext.Progress.WriteLine("Executing SubmitTimeSheet test cases");                
+        }
+
+        [TestCase, Order(3)]       
+        public void ApproveTimeSheet()
+        {
+            TestContext.Progress.WriteLine("Executing ApproveTimeSheet test cases");
+        }
+
+        [TestCase, Order(3)]
+        public void RejectTimeSheet()
+        {
+            TestContext.Progress.WriteLine("Executing RejectTimeSheet test cases");
+        }
+    }
+}
