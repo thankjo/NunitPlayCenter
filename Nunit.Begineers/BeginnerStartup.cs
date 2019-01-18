9fe3d46880053876b26357649f54ae46a02a6376
smitha <smithajojo@gmail.com>
2019-01-18 18:31:08 +0000

    Running test with nunit adapter

---
+++ Nunit.Begineers/BeginnerStartup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nunit.Begineers
+{
+    [NUnit.Framework.TestFixture]
+    public class BeginnerStartup
+    {
+        [NUnit.Framework.TestCase]
+        public void CheckingApplication()
+        {
+
+        }
+    }
+}
