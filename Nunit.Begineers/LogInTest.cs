00b756576b22f1d55d46ce3a05be6b20becb15dd
smitha <smithajojo@gmail.com>
2019-01-18 23:38:45 +0000

    #Parametrized testcase

---
+++ Nunit.Begineers/LogInTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nunit.Begineers
+{
+    [TestFixture]
+    class LogInTest
+    {
+        [TestCase("mary","mary@1","user")]
+        [TestCase("adminmary", "mary@1", "adminuser")]
+        [TestCase("superusermary", "mary@1", "superuser")]
+        public void ValidateCredentials(string username,string password,string role)
+        {
+
+        }
+
+        
+       
+        [TestCase]
+        public void PasswordRecovery(string email, string mobile)
+        {
+
+        }
+    }
+}
