f8574ee64d540b2d2368d576d7c5026c1fb6f8b0
smitha <smithajojo@gmail.com>
2019-01-19 11:49:22 +0000

    TestCaseSource

---
+++ Nunit.Begineers/DataDrivenTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nunit.Begineers
+{
+    [TestFixture]
+    class DataDrivenTests
+    {
+
+        static int[] ages = new int[] {11,12};
+
+        [Test, TestCaseSource(nameof(ages))]
+        public void ValidAgeGroups(int validage)
+        {
+
+        }
+    }
+}
